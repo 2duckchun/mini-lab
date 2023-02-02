c9d12f73140b461167984dc6c12d4e0ed6ac1ed8
2DC <kkts9308@gmail.com>
2023-02-02 18:30:18 +0900

    mysql 도입

--- 2-expressjs/controllers/shop.js
+++ 2-expressjs/controllers/shop.js
@@ -2,34 +2,45 @@ const Product = require('../models/product');
 const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, metaData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
-    });
-  });
+    })
+  })
+  .catch();
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId)
+  .then(([rows, metaData]) => {
+    console.log(rows)
     res.render('shop/product-detail', { 
-      product: product, 
-      pageTitle: product.title,
+      product: rows[0], 
+      pageTitle: rows[0].title,
       path: '/products'
     })
-  });
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, metaData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
-  });
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 };
 
 exports.getCart = (req, res, next) => {
@@ -66,7 +77,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
     res.redirect('/cart')
   })
 }
-
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
