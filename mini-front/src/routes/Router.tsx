bcc51576faec7404300d76c9e8263b4de6ed1660
2DC <kkts9308@gmail.com>
2023-08-07 15:33:03 +0900

    mini-front 라우터 컴포넌트 생성

---
+++ mini-front/src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "../user/Signin";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<Signin />} />
+        {/* <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/todo' element={<Todo />} />
+          <Route path='*' element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
