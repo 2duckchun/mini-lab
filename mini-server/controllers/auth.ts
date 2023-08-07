7a3700ab618baa2312942cd1e20b62aa65b7f4ff
2DC <kkts9308@gmail.com>
2023-08-07 16:04:16 +0900

    mini-server : Auth 컨트롤러 id 중복체크, nickname 중복체크, 인증정보 전송 로직 추가

--- mini-server/controllers/auth.ts
+++ mini-server/controllers/auth.ts
@@ -1,24 +1,111 @@
 // 모델을 컨트롤하는 컨트롤러 작성
 import User from "../models/user";
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
 import { Response, Request, NextFunction } from "express";
-import { sendEmail } from "../config/email";
+import { senderConfig, setSendEmail } from "../config/email";
 
-export const sendEmailToUserText = (req: Request, res: Response) => {
-  // sendEmail("kkts9308@gmail.com", String(123456));
-  return res.status(201).json({
-    statusCode: 201,
-    message: "success",
+export const sendEmail = (req: Request, res: Response) => {
+  const { userEmail, authCode } = req.body;
+  const smtpTransport = nodemailer.createTransport(senderConfig);
+  const mail = setSendEmail(userEmail, authCode);
+
+  smtpTransport.sendMail(mail, (error, response) => {
+    if (error) {
+      console.error(error);
+      res.status(421).json({
+        statusCode: 421,
+        message: "이메일이 전송되지 않았습니다.",
+      });
+    } else {
+      console.log(response);
+      res.status(200).json({
+        statusCode: 200,
+        message: "이메일이 전송되었습니다.",
+      });
+    }
+    smtpTransport.close();
   });
 };
 
-export const createUser = (req: Request, res: Response, next: NextFunction) => {
-  const { name, age, comment } = req.body;
+export const checkNickNameDuplicate = (req: Request, res: Response) => {
+  const { nickname } = req.body;
+
+  // @ts-ignore
+  User.findOne({
+    attributes: ["nickname"],
+    where: {
+      nickname,
+    },
+  })
+    .then((response) => {
+      if (response === null) {
+        res.status(200).json({
+          statusCode: 200,
+          message: "사용 가능한 닉네임입니다.",
+        });
+      } else {
+        res.status(409).json({
+          statusCode: 409,
+          message: "중복된 닉네임이 있습니다.",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        ...error,
+        statusCode: 500,
+        message: "잘못된 DB 접근입니다.",
+      });
+    });
+};
+
+export const checkIdDuplicate = (req: Request, res: Response) => {
+  const { userInputId } = req.body;
+
+  // @ts-ignore
+  User.findOne({
+    attributes: ["loginId"],
+    where: {
+      loginId: userInputId,
+    },
+  })
+    .then((response) => {
+      if (response === null) {
+        res.status(200).json({
+          statusCode: 200,
+          message: "사용 가능한 ID입니다.",
+        });
+      } else {
+        res.status(409).json({
+          statusCode: 409,
+          message: "중복된 ID가 있습니다.",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        ...error,
+        statusCode: 500,
+        message: "잘못된 DB 접근입니다.",
+      });
+    });
+};
+
+export const createUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { loginId, password, email, nickname } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 13);
 
   // @ts-ignore
   User.create({
-    name: name,
-    age: age,
-    comment: comment,
+    loginId: loginId,
+    password: hashedPassword,
+    email: email,
+    nickname: nickname,
   })
     .then((data) => {
       console.log("성공했어요!");
@@ -28,8 +115,10 @@ export const createUser = (req: Request, res: Response, next: NextFunction) => {
       });
     })
     .catch((error) => {
-      res.status(401).json({
-        statusCode: 401,
+      console.log(error);
+      res.status(500).json({
+        ...error,
+        statusCode: 500,
         message: "잘못된 DB 접근입니다.",
       });
     });
